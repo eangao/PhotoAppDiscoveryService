eeafc46c653378ff310c7526c487ab030cf2cbce
Elmar Angao <32320010+eangao@users.noreply.github.com>
2021-05-18 11:05:53 +0800

    Photo App Discovery Service Initial Setup

---
+++ src/main/java/com/appsdeveloper/photoapp/discovery/photoappdiscoveryservice/PhotoAppDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloper.photoapp.discovery.photoappdiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppDiscoveryServiceApplication.class, args);
+    }
+
+}
